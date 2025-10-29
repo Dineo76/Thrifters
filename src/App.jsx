f2141a312938a840a2dbfb2000b13e1596d12f94
Sibongile <sibongilearathokwane@gmail.com>
2025-10-29 11:04:36 +0200

    my navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-      
-    </>
-  )
+  return <Dashboard />;
 }
 
-export default App
+export default App;
+
+
